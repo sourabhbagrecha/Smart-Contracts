49411bdd8951c492f5232c53e1d566a0d08408f9
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2019-12-12 03:37:24 +0530

    Contracts made while learning Ethereum

---
+++ Lottery Contract/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+    'creek spell wet level cancel brick diet skin vacant engine universe cruise',
+    'https://rinkeby.infura.io/v3/e1fa40b52a424353865fabfe38406ca5'   
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+
+        console.log('Attempting to deploy from account', accounts[0]);
+    
+        const results = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: '0x' + bytecode })
+            .send({ gas: '1000000', from: accounts[0] });
+    
+        console.log(interface);
+        console.log('Contract deployed to: ', results.options.address);    
+    } catch (error) {
+        console.log(error);
+    }
+};
+deploy();
